a3b5ce9bdab213bd2bf9846128bcba68f1057e64
Khalid Al-Kurdi <Khalidalkurdi541@gmail.com>
2026-06-11 15:42:32 +0300

    refactor(payments): import PrismaModule and RealtimeModule

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { RealtimeModule } from '../realtime/realtime.module';
 
 @Module({
   controllers: [PaymentsController],
   providers: [PaymentsService],
+  imports: [PrismaModule, RealtimeModule],
   exports: [PaymentsService],
 })
 export class PaymentsModule {}
